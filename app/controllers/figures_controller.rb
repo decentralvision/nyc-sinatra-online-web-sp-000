592dae07171b2a915c3c8c17d2850cde4279a2a4
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-04-08 20:05:21 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,9 +1,9 @@
 class FiguresController < ApplicationController
   get '/figures' do
-   @figures = Figure.all
-   erb :'figures/index'
- end
- get '/figures/new' do
-   erb :'figures/new'
- end
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+    get '/figures/new' do
+    erb :'figures/new'
+  end
 end
