5d3254f35e73a9ff74e488a21b6a6facd1d91296
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-04-08 20:05:49 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -6,4 +6,12 @@ class FiguresController < ApplicationController
   get '/figures/new' do
     erb :'figures/new'
   end
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
 end
